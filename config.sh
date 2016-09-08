8dfd8ca3f418bbe17c9b76f9b375a5705b8ef4b9
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-08 00:15:58 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,3 +5,4 @@ zypper dup
 zypper in broadcom-wl broadcom-wl-kmp-default
 # reboot and configure wireless
 zypper in chromium krusader yakuake git gcc virtualbox
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
