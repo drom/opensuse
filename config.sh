895a075f599c4680ac34c70070a9d6a6be649958
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-07 23:56:11 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,3 +3,4 @@ zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_
 zypper dup
 # reboot
 zypper in broadcom-wl broadcom-wl-kmp-default
+zypper in chromium krusader yakuake
