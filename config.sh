1ab348d5429de1d1c19866663631a236ff8e8461
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-07 23:40:28 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-# tumbleweed fresh install
+# tumbleweed fresh install with kde5
 zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 sudo zypper dup
 
