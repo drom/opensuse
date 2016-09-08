3a81c861f1ff602149b98043e7e56f4a76321b12
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-07 23:46:31 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 # tumbleweed fresh install with kde5
-sudo zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
-sudo zypper dup
-
+zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+zypper dup
+# reboot
+zypper in broadcom-wl broadcom-wl-kmp-default
