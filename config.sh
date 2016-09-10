9c2fec5b58ad091d9fd21db96dff7c7cb569a7d0
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-09 22:30:12 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,6 +7,4 @@ zypper in broadcom-wl broadcom-wl-kmp-default
 zypper in chromium krusader yakuake git gcc virtualbox
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
 # hiDPI https://wiki.archlinux.org/index.php/HiDPI
-zypper in xdpyinfo
-xdpyinfo | grep -B2 resolution
-xrandr --dpi 144
+# System Settings -> Fint -> Force DPI: 144
