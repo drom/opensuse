bdae54aa11fbbab0c309e9f3905fd4ee61da783f
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-09-09 22:19:26 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,3 +6,7 @@ zypper in broadcom-wl broadcom-wl-kmp-default
 # reboot and configure wireless
 zypper in chromium krusader yakuake git gcc virtualbox
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+# hiDPI https://wiki.archlinux.org/index.php/HiDPI
+zypper in xdpyinfo
+xdpyinfo | grep -B2 resolution
+xrandr --dpi 144
