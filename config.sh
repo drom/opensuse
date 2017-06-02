7d1d67d67c72098b388fc3525bf9c3d740250eed
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-06-01 23:29:03 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 zypper dup
 # reboot for new kernel
+# if Broadcom card used
 zypper in broadcom-wl broadcom-wl-kmp-default
 # reboot and configure wireless
 zypper in chromium krusader yakuake git gcc virtualbox
