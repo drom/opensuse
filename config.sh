c32d2ae9c3d509b311ffdb9cb6b91a71fdfba1c5
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-09-21 22:55:51 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_
 zypper dup
 # reboot for new kernel
 # if Broadcom card used
-zypper in broadcom-wl broadcom-wl-kmp-default
+zypper in iw broadcom-wl broadcom-wl-kmp-default
 # reboot and configure wireless
 zypper in chromium krusader yakuake vlc git gcc virtualbox kdiff3 rar zip krename ktorrent inkscape llvm clang
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
