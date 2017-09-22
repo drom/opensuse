56254218a4f15125134d6c04c31e7b15e6157498
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-09-21 22:48:30 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,3 +9,7 @@ zypper in chromium krusader yakuake vlc git gcc virtualbox kdiff3 rar zip krenam
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 # hiDPI https://wiki.archlinux.org/index.php/HiDPI
 # System Settings -> Fint -> Force DPI: 144
+#
+# Ham Radio
+zypper ar -f -n hamradio https://download.opensuse.org/repositories/hamradio/openSUSE_Tumbleweed/ hamradio
+zypper in gqrx gnuradio rtl-sdr
