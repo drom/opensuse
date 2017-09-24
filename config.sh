0a4520286b6432514f0b66e3a7cd615621c7dfbe
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-09-23 21:43:05 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,10 @@ zypper in iw broadcom-wl broadcom-wl-kmp-default
 zypper in chromium krusader yakuake vlc git gcc virtualbox kdiff3 rar zip krename ktorrent inkscape llvm clang
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 # hiDPI https://wiki.archlinux.org/index.php/HiDPI
-# System Settings -> Fint -> Force DPI: 144
+# System Settings -> Font -> Force DPI: 144
+#
+# Russian
+# Syatem Settings -> Input Devices -> Keyboard -> Layouts -> Configure layouts -> +Add -> Russian
 #
 # Ham Radio
 zypper ar -f -n hamradio https://download.opensuse.org/repositories/hamradio/openSUSE_Tumbleweed/ hamradio
